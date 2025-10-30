373104f413e65ec741ef69cdefbbeeb70818b6ab
subhan_2007 <rokvlili26@gmail.com>
2025-10-30 11:42:10 +0400

    first commit

---
+++ src/test/java/az/ingress/msdemo/MsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.msdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
